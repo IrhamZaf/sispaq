590fdcb32c0b89c7cf306240f5d2b25498a1156a
IrhamZaf <85091440+IrhamZaf@users.noreply.github.com>
2026-05-24 00:37:37 +0800

    Update build files and .gitignore; add new index.html and manifest.json for Rollup bundle, and include various asset files.

---
+++ public/build/assets/jquery-D4bU1gXx.js
@@ -0,0 +1,1 @@
+import{a as r}from"./_commonjsHelpers-_d1bhYXs.js";import{r as o}from"./jquery-DKenW1nw.js";var e=o();const s=r(e);export{s as j};
---
+++ public/build/assets/jquery-Dl2WjdB1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./jquery-D4bU1gXx.js";import"./_commonjsHelpers-_d1bhYXs.js";import"./jquery-DKenW1nw.js";try{window.jQuery=window.$=r}catch{}
